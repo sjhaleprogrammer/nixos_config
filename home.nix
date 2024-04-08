a98f65b0655e7187ab7d1318692310c4b58dd4f9
samuel <samworlds1231337@gmail.com>
2024-04-08 01:59:54 +0000

    changed zsh

--- home.nix
+++ home.nix
@@ -31,118 +31,10 @@
   };
 
   
-
-  # starship - an customizable prompt for any shell
-  programs.starship = {
+  programs.oh-my-posh = {
     enable = true;
-    settings = {
+    useTheme = "powerlevel10k_rainbow";
 
-      add_newline = false;
-      command_timeout = 1000;
-      format = "$os$username$hostname$kubernetes$directory$git_branch$git_status";
-      
-      character = {
-        success_symbol = "";
-        error_symbol = "";
-
-      };
-
-      os = {
-        format = "[$symbol](bold white) ";
-        disabled = false;
-      };
-
-      os.symbols = {
-        Windows = "";
-        Arch = "󰣇";
-        Ubuntu = "";
-        Macos = "󰀵";
-      };
-
-      username = {
-        style_user = "white bold";
-        style_root = "black bold";
-        format = "[$user]($style) ";
-        disabled = false;
-        show_always = true;
-      };
-
-      hostname = {
-        ssh_only = false;
-        format = "on [$hostname](bold yellow) ";
-        disabled = false;
-      };
-
-      directory = {
-        truncation_length = 1;
-        truncation_symbol = "…/";
-        home_symbol = "󰋜 ~";
-        read_only_style = "197";
-        read_only = "  ";
-        format = "at [$path]($style)[$read_only]($read_only_style) ";
-
-      };
-
-      git_branch = {
-        symbol = " ";
-        format = "via [$symbol$branch]($style)";
-        truncation_symbol = "…/";
-        style = "bold green";
-      };
-
-
-      git_status = {
-        format = "[$all_status$ahead_behind]($style) ";
-        style = "bold green";
-        conflicted = "🏳";
-        up_to_date = "";
-        untracked = " ";
-        ahead = "⇡\${count}";
-        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
-        behind = "⇣\${count}";
-        stashed = " ";
-        modified = " ";
-        staged = "[++\($count\)](green)";
-        renamed = "襁 ";
-        deleted = " ";
-
-      };
-
-      kubernetes = {
-        format = "via [󱃾 $context\($namespace\)](bold purple) ";
-        disabled = false;
-      };
-
-      vagrant = {
-        disabled = true;
-      };  
-
-      docker_context = {
-        disabled = true;
-      };
-
-      helm = {
-        disabled = true;
-      };
-      
-      python = {
-        disable = true;
-      };
-
-      nodejs = {
-        disable = true;
-      };
-
-      ruby = {
-        disable = true;
-      };
-      
-      terraform = {
-        disable = true;
-      };
-
-
-    };
   };
 
   programs.zsh = {
@@ -150,15 +42,14 @@
     enableCompletion = true;
     autosuggestion.enable  = true;
     syntaxHighlighting.enable = true;
-    oh-my-zsh = {
-      enable = true;
-    };
-
+    
     initExtra = ''
       (cat /home/samuel/.cache/wal/sequences &)
-      eval "$(starship init zsh)"
     '';
   };
+
+
+
 
  
 }
