18a3bd8fbecfcc6e01b4317fba5fae3330f821e6
samuel <samworlds1231337@gmail.com>
2024-11-11 15:35:37 -0500

    formatters

--- neovim.nix
+++ neovim.nix
@@ -1,73 +1,80 @@
 { pkgs, ... }:
 
-
 {
-        programs.neovim = {
-                  enable = true;
-		  defaultEditor = true;
-		  vimAlias = true;
-                  configure = {
-                    customRC = ''
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    configure = {
+      customRC = ''
 
-		      autocmd VimEnter * highlight Normal guibg=none ctermbg=none
-		      autocmd VimEnter * Neotree 
+        autocmd VimEnter * highlight Normal guibg=none ctermbg=none
+        autocmd VimEnter * Neotree 
+        luafile ${./neovim.lua}
 
-                      set number
-                      set shiftwidth=8
-                      set tabstop=8
-                      set expandtab
-                      set smartindent
+        set number
+        set shiftwidth=8
+        set tabstop=8
+        set expandtab
+        set smartindent
 
-                      colorscheme dracula
-
-
-                      if &diff
-                        colorscheme blue
-                      endif
+        colorscheme dracula
 
 
-                      luafile ${./neovim.lua}
-
-                    '';
-
-                    packages.myVimPackage = with pkgs.vimPlugins; {
-                      start = [
-
-                        dracula-nvim
-                        airline
-
-                        cmp_luasnip
-                        cmp-nvim-lsp
-                        cmp-buffer
-                        cmp-path
-                        cmp-cmdline
-
-                        indent-blankline-nvim
-                        gitsigns-nvim
-                        
-                        neo-tree-nvim
-                        nvim-web-devicons
+        if &diff
+          colorscheme blue
+        endif
 
 
-                        telescope-nvim
-                        codeium-nvim
-                        conform-nvim
-                        nvim-treesitter
-                        nvim-lspconfig
-                        
-                      ];
-                    };
-                  };
+      '';
 
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [
 
-        };
+          dracula-nvim
+          airline
 
+          cmp_luasnip
+          cmp-nvim-lsp
+          cmp-buffer
+          cmp-path
+          cmp-cmdline
 
-        environment.systemPackages = with pkgs; [
-                wl-clipboard
-                gcc
-                tree-sitter
-                nodejs-slim
+          indent-blankline-nvim
+          gitsigns-nvim
+
+          neo-tree-nvim
+          nvim-web-devicons
+
+          telescope-nvim
+          codeium-nvim
+          conform-nvim
+          nvim-treesitter
+          nvim-lspconfig
+
         ];
+      };
+    };
+
+  };
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    gcc
+    tree-sitter
+    nodejs-slim
+
+    #ls
+    pyright
+    nil
+    lua-language-server
+
+    #formatters
+    nixfmt-classic
+    codespell
+    isort
+    black
+
+  ];
 
 }
